0f8e0a21a82c52842ef38057c679e48189fdcde5
素流 <bangzhe.lbz@alibaba-inc.com>
2016-12-16 15:37:30 +0800

    改动

--- TBBZ_swift/ViewController.swift
+++ TBBZ_swift/ViewController.swift
@@ -14,41 +14,41 @@ class ViewController: UIViewController {
   var nextY=80
   var labels=[UILabel]()
   var weatherLabel=[UILabel]()
-  let mainwidth:CGFloat = UIScreen.mainScreen().bounds.width
+  let mainwidth:CGFloat = UIScreen.main.bounds.width
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     let weatherBtn=UIButton(frame:CGRect(x:mainwidth/2-75, y:30, width:150, height:70))
-    weatherBtn.setTitle("天气预报", forState:UIControlState.Normal)
+    weatherBtn.setTitle("天气预报", for:UIControlState())
     weatherBtn.titleLabel?.font = UIFont(name:"STKaiti", size:30)
-    weatherBtn.titleLabel?.textAlignment = NSTextAlignment.Center
-    weatherBtn.setTitleColor(UIColor.blueColor(), forState:UIControlState.Normal)
-    weatherBtn.setTitleColor(UIColor.greenColor(), forState:UIControlState.Highlighted)
-    weatherBtn.addTarget(self, action:#selector(ViewController.weatherButton), forControlEvents:UIControlEvents.TouchUpInside)
+    weatherBtn.titleLabel?.textAlignment = NSTextAlignment.center
+    weatherBtn.setTitleColor(UIColor.blue, for:UIControlState())
+    weatherBtn.setTitleColor(UIColor.green, for:UIControlState.highlighted)
+    weatherBtn.addTarget(self, action:#selector(ViewController.weatherButton), for:UIControlEvents.touchUpInside)
     self.view.addSubview(weatherBtn);
   }
 
   func weatherButton() {
-    let url = NSURL(string : "http://www.weather.com.cn/data/sk/101010100.html")
-    let data = NSData(contentsOfURL: url!)
-    var json:AnyObject! = nil
+    let url = URL(string : "http://www.weather.com.cn/data/sk/101010100.html")
+    let data = try? Data(contentsOf: url!)
+    var json:Any! = nil
     do {
-      json = try NSJSONSerialization .JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments)
+      json = try JSONSerialization.jsonObject(with: data!, options:JSONSerialization.ReadingOptions.allowFragments)
     }catch {
       print("error")
     }
     let label:UILabel = UILabel(frame:CGRect(x: 10, y: 110, width: mainwidth, height: 40))
-    let weatherinfo = json.objectForKey("weatherinfo")
+    let weatherinfo = (json as! NSDictionary).object(forKey: "weatherinfo")!
     
-    label.text = "city: \((weatherinfo?.objectForKey("city"))!)  time: \((weatherinfo?.objectForKey("time"))!)  rain: \((weatherinfo?.objectForKey("rain"))!)"
+    label.text = "city: \(((weatherinfo as AnyObject).object(forKey: "city"))!)  time: \(((weatherinfo as AnyObject).object(forKey: "time"))!)  rain: \(((weatherinfo as AnyObject).object(forKey: "rain"))!)"
     label.font = UIFont(name:"STKaiti", size:15)
-    label.textAlignment = NSTextAlignment.Center
+    label.textAlignment = NSTextAlignment.center
     self.labels.append(label)
     self.view.addSubview(label)
   }
   
-  override func viewWillAppear(animated: Bool) {
+  override func viewWillAppear(_ animated: Bool) {
     
   }
 }
